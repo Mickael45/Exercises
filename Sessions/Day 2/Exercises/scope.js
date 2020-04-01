468395b72544ce838dc59c2fc76724cb363372b7
Rosietesting <isantisoftwareTesting@gmail.com>
2020-04-01 09:16:46 +0100

    Rosibel Commi 1April

--- Sessions/Day 2/Exercises/scope.js
+++ Sessions/Day 2/Exercises/scope.js
@@ -13,7 +13,9 @@ function increaseCountByARandomValue() {
   count += randomValue;
   // += is just a shorter way to write "count = count + randomValue"
   // This also works with -, /, * and %
+
 }
+
 
 function decreaseCountByARandomValue() {
   const randomValue = Math.random();
@@ -24,18 +26,31 @@ function decreaseCountByARandomValue() {
 
   const safeValue = makeSureValueIsNotLowerThanZero(randomValue);
   count -= randomValue;
+ 
+
 }
+
 
 increaseCountByARandomValue();
 decreaseCountByARandomValue();
 
-// is increaseCountByARandomValue accessible in the file's global scope ?
-// is decreaseCountByARandomValue accessible in the file's global scope ?
-// is makeSureValueIsNotHigherThanAHundred accessible in the file's global scope ?
-// is makeSureValueIsNotHigherThanAHundred accessible in the increaseCountByARandomValue function scope ?
-// is makeSureValueIsNotHigherThanAHundred accessible in the decreaseCountByARandomValue function scope ?
-// is increaseCountByARandomValue accessible in the decreaseCountByARandomValue function scope ?
-// is increaseCountByARandomValue accessible in the increaseCountByARandomValue function scope ?
 
+// is increaseCountByARandomValue accessible in the file's global scope ?
+// RR : Yes, it can be  accessed anywhere
+// is decreaseCountByARandomValue accessible in the file's global scope ?
+// RR : Yes, it can be  accessed anywhere
+// is makeSureValueIsNotHigherThanAHundred accessible in the file's global scope ?
+// RR : No. Only in the scope of makeSureValueIsNotHigherThanAHundred
+// is makeSureValueIsNotHigherThanAHundred accessible in the increaseCountByARandomValue function scope ?
+// RR: Yes
+// is makeSureValueIsNotHigherThanAHundred accessible in the decreaseCountByARandomValue function scope ?
+//  RR: No
+// is increaseCountByARandomValue accessible in the decreaseCountByARandomValue function scope ?
+//RR :Yes
+// is increaseCountByARandomValue accessible in the increaseCountByARandomValue function scope ?
+//RR : No
 // Where is the count variable accessible from ?
+//RR : anywhere
 // Where is decreaseCountByARandomValue's randomValue and safeValue accessible from ?
+//RR  : safeValue  = decreaseCountByARandomValue and increaseCountByARandomValue
+//randomValue = decreaseCountByARandomValue and increaseCountByARandomValue
---
+++ Sessions/Day 2/RosibelExercice.js
@@ -0,0 +1,2 @@
+let textInfo = 'Little bonus at the end if you successfuly  solve the black magic logic.'
+console.log(textInfo);
